29515cb57cabc77db919bf725034db7d56bd1ddf
Steven Todd <steven@flare-photography.com>
2022-07-26 23:12:01 +0100

    Print the difference between the smallest and largest numbers

--- numbers_hw.py
+++ numbers_hw.py
@@ -1,8 +1,22 @@
 # For the following list of numbers:
 
+from cgitb import small
+
+
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # Print only even numbers
 for n in numbers:
     if n % 2 == 0:
         print(n)
+        
+numbers.sort()
+smallest = numbers[0]
+largest = numbers[-1]
+difference = largest - smallest
+# print(smallest)
+# print(largest)
+print()
+print()
+
+print(f"The difference between the smallest number of {smallest} and the largest number of {largest} is {difference}")
